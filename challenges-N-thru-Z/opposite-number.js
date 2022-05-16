e0430fd21d21a909bdb0b88dde60d25c37950a14
James Peeler <james.peeler.a@gmail.com>
2022-05-16 13:56:22 -0400

    opposite number

---
+++ challenges-N-thru-Z/opposite-number.js
@@ -0,0 +1,7 @@
+//Parameters: a whole number, positive or negative
+//Return: the given number, but the opposite polarity
+
+function opposite(number) {
+    //convert number and return value
+    return number - number * 2
+  }
